9e47aa3f8d2e9dd8d0dcbc35ea31ba46c6bbc657
alexander-carrels <alexander.carrels@outlook.com>
2024-07-20 09:24:55 +0200

    add resize scripts

---
+++ resize_low.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+resize-images --target_mp 0.2 --output_sub_directory low --suffix _low
---
+++ resize_medium.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+resize-images --target_mp 2 --output_sub_directory med --suffix _med
